7c6037c1b5d8658e7a8d4835cb5e0777043168d9
MiccDev <54465951+MiccDev@users.noreply.github.com>
2022-07-05 11:11:20 +1200

    Revert "Initial project structure + flip cmd"

--- src/api/coin.ts
+++
@@ -1,8 +0,0 @@
-import { getRandomBetween } from "./utils/mathUtils";
-
-function flip(): string {
-    if (getRandomBetween(0,1) == 0) { return "Heads"; }
-    else { return "Tails"; }
-}
-
-export { flip }
--- src/api/discord/events/readyEvent.ts
+++
@@ -1,8 +0,0 @@
-import { RunFunction } from "../interfaces/event";
-import { Bot } from "../client/client";
-
-export const run: RunFunction = async (client: Bot) => {
-    console.log(client.user.tag + " has logged in!")
-}
-
-export const name: string = 'ready';
